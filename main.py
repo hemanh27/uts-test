dbe4c29ffcf8b8f570dd6bb551322c6f58e14bd7
hemanh27 <hemanh27@gmail.com>
2024-03-10 16:05:22 +0530

    implement threading

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import threading
 from src.password_validator import validate_passwords
 from src.circular_queue import CircularQueue
 
 
-def main():
+def program1():
     with open('input/passwords.txt', 'r') as file:
         passwords = file.read().split(',')
     accepted_passwords = validate_passwords(passwords)
     print("Accepted passwords:", ",".join(accepted_passwords))
 
+
+def program2():
     queue = CircularQueue()
     for i in range(1, 10):
         queue.enqueue(i)
     queue.display()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    main()
+def main():
+    # Create threads for each program
+    thread1 = threading.Thread(target=program1)
+    thread2 = threading.Thread(target=program2)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Start the threads
+    thread1.start()
+    thread2.start()
+
+    # Wait for all threads to complete
+    thread1.join()
+    thread2.join()
+
+
+if __name__ == "__main__":
+    main()
